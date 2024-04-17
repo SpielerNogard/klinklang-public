04548c6ea2c51dac1e00ea7c3da4d81ad739638d
Xerock <gibson.daemon@gmail.com>
2024-04-17 14:30:23 -0400

    Update configgenerator.py

--- configgenerator.py
+++ configgenerator.py
@@ -22,7 +22,7 @@ DOCKERCOMPOSE = {
 
 
 def input_with_default(prompt, default):
-    result = input(f"{prompt}({default=})")
+    result = input(f"{prompt}({default})")
     if not result:
         return default
     return result
